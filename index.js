12224536f26ba861a8958bea7ece51ceb61a4f05
Padredilg <padredilg@knights.ucf.edu>
2021-09-17 11:08:06 -0400

    create db connection and table prototypes

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
+const db = require('./db/connection');
+
 //create prompts
-const cTable = require('console.table');
-const table = cTable.getTable([
-  {
-    name: 'foo',
-    age: 10
-  }, {
-    name: 'bar',
-    age: 20
-  }
-]);
+// const cTable = require('console.table');
+// const table = cTable.getTable([
+//   {
+//     name: 'foo',
+//     age: 10
+//   }, {
+//     name: 'bar',
+//     age: 20
+//   }
+// ]);
+
+db.connect(err => {
+    if (err) throw err;
+    console.log('Database connected.');
+});

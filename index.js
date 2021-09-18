2962b3a2bb946001153f6bbc8162f1c83d1b4101
Padredilg <padredilg@knights.ucf.edu>
2021-09-17 22:58:02 -0400

    fixed prompts

--- index.js
+++ index.js
@@ -8,97 +8,90 @@ const action = () =>{
             name: 'action',
             message: 'What would you like to do now?',
             choices: [
-                'view all departments',
-                'view all roles',
-                'view all employees',
-                'add a department',
-                'add a role',
-                'add an employee',
-                'update an employee role',
-                'end application'
+                'View all departments',
+                'View all roles',
+                'View all employees',
+                'Add a department',
+                'Add a role',
+                'Add an employee',
+                'Update an employee role',
+                'Exit'
             ]
         }
     )
     .then(decision => {
         //check what decision is and act accordingly
-        if(decision.action === 'view all departments'){
+        if(decision.action === 'View all departments'){
             viewDepartments();
         }
-        else if(decision.action === 'view all roles'){
+        else if(decision.action === 'View all roles'){
             viewRoles();
         }
-        else if(decision.action === 'view all employees'){
+        else if(decision.action === 'View all employees'){
             viewEmployees();
         }
-        else if(decision.action === 'add a department'){
+        else if(decision.action === 'Add a department'){
             addDepartment();
         }
-        else if(decision.action === 'add a role'){
+        else if(decision.action === 'Add a role'){
             addRole();
         }
-        else if(decision.action === 'add an employee'){
+        else if(decision.action === 'Add an employee'){
             addEmployee();
         }
         
-        else if(decision.action === 'update an employee role'){
+        else if(decision.action === 'Update an employee role'){
             updateEmployee();
         }
         else{
-            endApp();
+            exit();
         }
     })
 }
 
 const viewDepartments = () =>{
     console.log('show all departments')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const viewRoles = () =>{
     console.log('show all Roles')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const viewEmployees = () =>{
     console.log('show all Employees')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const addDepartment = () =>{
     console.log('add department')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const addRole = () =>{
     console.log('add Role')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const addEmployee = () =>{
     console.log('add Employee')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
 const updateEmployee = () =>{
     console.log('update Employee')
-        .then(() => {
-            action();
-        });
+    
+    action();
 }
 
-const endApp = () =>{
+const exit = () =>{
     console.log('Thank you for your time!');
 }
 
@@ -111,7 +104,6 @@ db.connect(err => {
     action();
 });
 
-//create prompts
 // const cTable = require('console.table');
 // const table = cTable.getTable([
 //   {
